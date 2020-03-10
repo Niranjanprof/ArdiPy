b567219f9201b48223d73cafdc1a78eb856d7c1e
Niranjanprof <48713926+Niranjanprof@users.noreply.github.com>
2020-03-10 22:40:44 +0530

    Add files via upload

---
+++ Ardipy/Ardipy/Ardipy.ino
@@ -0,0 +1,13 @@
+// Niranjan prof
+//https://github.com/Niranjanprof
+//Mailme @ niranjannb7777@gmail.com
+
+void setup() {
+  Serial.begin(9600);//Serial Monitor of 9600 baud rate 
+
+}
+
+void loop() {
+  Serial.println("HI") // print HI in Serial Monitor
+
+}
---
+++ Ardipy/Ardipy/Ardipy.py
@@ -0,0 +1,11 @@
+import serial  # pip install serial   or pip install pyserial in    cmd or terminal or pycharm virtual terminal
+import time
+ser = serial.Serial('COM6',9600)   #specify your port
+
+time.sleep(2)
+time.sleep(2)
+
+a = ser.readline()  #Read line from Serial Monitor
+a = a.decode()
+a = a.rstrip()
+print(a)   #It will print HI
